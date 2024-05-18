c576561d5a162f937c1bf3e5842bb39d9769d780
isaui <isadestroyed17@gmail.com>
2024-05-19 02:12:35 +0700

    feat: websocket connection to realtime update

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
           {
             source: '/api-gateway-proxy/:path*',
             destination: 'https://api-gateway-specialitystore.up.railway.app/:path*' // Proxy ke produk eksternal
+          },
+          {
+            source: '/product-development/:path*',
+            destination: 'http://localhost:8080/:path*'
           }
         ]
       }
