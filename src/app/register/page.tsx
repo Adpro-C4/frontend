f7af16627081897eb365c8dd0dcbec581c63015b
isaui <isadestroyed17@gmail.com>
2024-05-18 19:46:39 +0700

    feat: register 75%

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterModule from "@/components/modules/RegisterModule";
 
 const Page = () => {
     return (
-        <main className="flex flex-col items-center w-screen min-h-screen bg-white">
+        <main className="flex flex-col items-center w-screen min-h-screen bg-pink-200">
           <RegisterModule/>
         </main>
       );
